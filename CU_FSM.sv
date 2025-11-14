39fee4fb185de4b78a465ceb46b96fab004373b8
linvict321 <86497178+linvict321@users.noreply.github.com>
2025-11-14 13:00:09 -0800

    Enhance comments and formatting in CU_FSM
    Refactor state machine comments and improve readability.

--- CU_FSM.sv
+++ CU_FSM.sv
@@ -11,7 +11,7 @@ module CU_FSM(
     output logic reset
     );
 
-    typedef enum logic [1:0] {ST_init, ST_fetch, ST_exec} STATES;
+    typedef enum logic [1:0] {ST_init, ST_fetch, ST_exec} STATES; //all states
     STATES PS, NS;
 
     always_ff @(posedge CLK) begin
@@ -19,7 +19,7 @@ module CU_FSM(
         else     PS <= NS;
     end
 
-    always_comb begin
+    always_comb begin //set all inputs to zero
         PCWrite  = 0;
         regWrite = 0;
         memWE2   = 0;
@@ -30,31 +30,31 @@ module CU_FSM(
 
         case (PS)
 
-            ST_init: begin
-                reset = 1;
+            ST_init: begin //initial state
+                reset = 1; 
                 NS = ST_fetch;
             end
 
-            ST_fetch: begin
-                memRDEN1 = 1;
+            ST_fetch: begin //fetch state
+                memRDEN1 = 1; 
                 PCWrite  = 1;
                 NS = ST_exec;
             end
 
-            ST_exec: begin
+            ST_exec: begin //execute state
                 unique case (opcode)
-                    7'b0110011: regWrite = 1; // R-type
-                    7'b0000011: begin         // LW
+                    7'b0110011: regWrite = 1; // rtype
+                    7'b0000011: begin         // lw
                         memRDEN2 = 1;
                         regWrite = 1;
                     end
-                    7'b0010011: regWrite = 1; // ADDI etc
-                    7'b0100011: memWE2   = 1; // SW
-                    7'b0110111: regWrite = 1; // LUI
+                    7'b0010011: regWrite = 1; // addi
+                    7'b0100011: memWE2   = 1; // sw
+                    7'b0110111: regWrite = 1; // lui
                 endcase
 
-                NS = ST_fetch;
-            end
+                NS = ST_fetch; //end
+            end 
         endcase
     end
 endmodule
